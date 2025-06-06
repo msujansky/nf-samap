e44e2fba3b105f2f2fbe4439c402789b9914dff5
RyanSonder <ryansonderpersonal@gmail.com>
2025-06-06 15:13:43 -0400

    cleaned useless map inputs

--- scripts/run_samap.py
+++ scripts/run_samap.py
@@ -16,10 +16,8 @@ class Args(NamedTuple):
     """ Command-line arguments """
     species1: str
     h5ad1: Path
-    map1to2: Path
     species2: str
     h5ad2: Path
-    map2to1: Path
 
 
 # --------------------------------------------------
@@ -40,11 +38,6 @@ def get_args() -> Args:
                         type=Path,
                         help='First species h5ad file')
     
-    parser.add_argument('map1to2',
-                        metavar='map1to2',
-                        type=Path,
-                        help='Mapping from species 1 to species 2')
-    
     parser.add_argument('species2',
                         metavar='s2',
                         type=str,
@@ -54,20 +47,13 @@ def get_args() -> Args:
                         metavar='h5ad2',
                         type=Path,
                         help='Second species h5ad file')
-    
-    parser.add_argument('map2to1', 
-                        metavar='map2to1',
-                        type=Path,
-                        help='Mapping from species 2 to species 1')
 
     args = parser.parse_args()
 
     return Args(args.species1,
                 args.h5ad1,
-                args.map1to2,
                 args.species2,
-                args.h5ad2,
-                args.map2to1)
+                args.h5ad2,)
 
 
 # --------------------------------------------------
@@ -75,22 +61,20 @@ def main() -> None:
     """ Run SAMap with command-line input """
 
     args = get_args()
-    
+
+    # Species 1 
     species1 = args.species1
     h5ad1 = str(args.h5ad1)
-    map1to2 = str(args.map1to2)
-    
+    # Species 2
     species2 = args.species2
     h5ad2 = str(args.h5ad2)
-    map2to1 = str(args.map2to1)
-    
 
     print(f'Running SAMAP for {species1} vs {species2}')
     print(f'Input files: {h5ad1}, {h5ad2}')
-    print(f'Mapping files: {map1to2}, {map2to1}')
     
     
-    filenames = {'hy':h5ad1,'pl':h5ad2}
+    filenames = {species1:h5ad1,species2:h5ad2}
+    
     sm = SAMAP(
         filenames,
         f_maps = 'example_data/maps/',
@@ -98,15 +82,9 @@ def main() -> None:
     )
     
     sm.run()
-    
-    
-    # keys = {'pl':'cluster','hy':'Cluster','sc':'tissue'}
-    # D,MappingTable = get_mapping_scores(sm,keys,n_top = 0)
     save_samap(sm, 'example_data/samap_output')
-    
-    
-    
-    
+
+
 # # --------------------------------------------------
 # def run_samap(species1: str, h5ad1: TextIO, map1to2: TextIO,
 #             species2: str, h5ad2: TextIO, map2to1: TextIO) -> None:
