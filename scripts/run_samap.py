8f16a1377a65afe818dd409eed2e8b3e184e1b99
RyanSonder <ryansonderpersonal@gmail.com>
2025-06-06 15:29:44 -0400

    abstracted dict building, removed commented code

--- scripts/run_samap.py
+++ scripts/run_samap.py
@@ -69,65 +69,25 @@ def get_args() -> Args:
 # --------------------------------------------------
 def main() -> None:
     """ Run SAMap with command-line input """
-
     args = get_args()
     
-    map_path = args.map_path
-
-    # Species 1 
-    species1 = args.species1
-    h5ad1 = str(args.h5ad1)
-    # Species 2
-    species2 = args.species2
-    h5ad2 = str(args.h5ad2)
-
-    print(f'Running SAMAP for {species1} vs {species2}')
-    print(f'Input files: {h5ad1}, {h5ad2}')
-    
-    
-    filenames = {species1:h5ad1,species2:h5ad2}
-    
     sm = SAMAP(
-        filenames,
-        f_maps = map_path,
+        build_h5ad_dict(args),
+        f_maps = args.map_path,
         save_processed=True #if False, do not save the processed results to `*_pr.h5ad`
     )
     
     sm.run()
     save_samap(sm, 'example_data/samap_output')
 
-
-# # --------------------------------------------------
-# def run_samap(species1: str, h5ad1: TextIO, map1to2: TextIO,
-#             species2: str, h5ad2: TextIO, map2to1: TextIO) -> None:
-#     """ Run SAMAP with the provided arguments """
+# --------------------------------------------------
+def build_h5ad_dict(args: Args) -> dict:
+    """ Build a dictionary of h5ad files for SAMAP """
+    return {
+        args.species1: str(args.h5ad1),
+        args.species2: str(args.h5ad2),
+    }
     
-#     # Pack the input parameters into a dictionary
-#     filenames = build_input_dict(species1, h5ad1, species2, h5ad2)
-    
-#     # Create the SAMAP object
-#     sm = SAMAP(
-#         filenames,
-#         f_maps='/home/ryan/git/github/ryansonder/my-samap/example_data/maps/hypl'
-#     )
-    
-#     sm.run(pairwise=True)
-#     samap = sm.samap
-    
-    
-#     # Run the SAMAP mapping
-    
-    
-# # --------------------------------------------------
-    
-# def build_input_dict(species1: str, h5ad1: TextIO,
-#                species2: str, h5ad2: TextIO) -> dict:
-#     """ Build a dictionary of input parameters for SAMAP """
-#     return {
-#         species1: h5ad1,
-#         species2: h5ad2,
-#     }
-
 
 # --------------------------------------------------
 if __name__ == '__main__':
