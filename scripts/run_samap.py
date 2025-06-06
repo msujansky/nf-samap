12d2da8cc8b42d914b07077ac9b216dfb913fa25
RyanSonder <ryansonderpersonal@gmail.com>
2025-06-06 14:38:41 -0400

    removed some hardcode

--- scripts/run_samap.py
+++ scripts/run_samap.py
@@ -6,26 +6,20 @@ Purpose: Run SAMAP with command-line input
 """
 
 import argparse
+from pathlib import Path
 from samap.mapping import SAMAP
-from samap.analysis import (get_mapping_scores, GenePairFinder,
-                            sankey_plot, chord_plot, CellTypeTriangles, 
-                            ParalogSubstitutions, FunctionalEnrichment,
-                            convert_eggnog_to_homologs, GeneTriangles)
-from samalg import SAM
 from samap.utils import save_samap
-import pandas as pd
-import matplotlib.pyplot as plt
 from typing import NamedTuple, TextIO
 
 
 class Args(NamedTuple):
     """ Command-line arguments """
     species1: str
-    h5ad1: TextIO
-    map1to2: TextIO
+    h5ad1: Path
+    map1to2: Path
     species2: str
-    h5ad2: TextIO
-    map2to1: TextIO
+    h5ad2: Path
+    map2to1: Path
 
 
 # --------------------------------------------------
@@ -43,12 +37,12 @@ def get_args() -> Args:
     
     parser.add_argument('h5ad1',
                         metavar='h5ad1',
-                        type=argparse.FileType('rt'),
+                        type=Path,
                         help='First species h5ad file')
     
     parser.add_argument('map1to2',
                         metavar='map1to2',
-                        type=argparse.FileType('rt'),
+                        type=Path,
                         help='Mapping from species 1 to species 2')
     
     parser.add_argument('species2',
@@ -58,12 +52,12 @@ def get_args() -> Args:
 
     parser.add_argument('h5ad2',
                         metavar='h5ad2',
-                        type=argparse.FileType('rt'),
+                        type=Path,
                         help='Second species h5ad file')
     
     parser.add_argument('map2to1', 
                         metavar='map2to1',
-                        type=argparse.FileType('rt'),
+                        type=Path,
                         help='Mapping from species 2 to species 1')
 
     args = parser.parse_args()
@@ -81,21 +75,25 @@ def main() -> None:
     """ Run SAMap with command-line input """
 
     args = get_args()
+    
+    
     species1 = args.species1
-    h5ad1 = args.h5ad1
-    map1to2 = args.map1to2
+    h5ad1 = str(args.h5ad1)
+    map1to2 = str(args.map1to2)
+    
     species2 = args.species2
-    h5ad2 = args.h5ad2
-    map2to1 = args.map2to1
+    h5ad2 = str(args.h5ad2)
+    map2to1 = str(args.map2to1)
+    
 
     print(f'Running SAMAP for {species1} vs {species2}')
-    print(f'Input files: {h5ad1.name}, {h5ad2.name}')
-    print(f'Mapping files: {map1to2.name}, {map2to1.name}')
+    print(f'Input files: {h5ad1},              {h5ad2}')
+    print(f'Mapping files: {map1to2},                {map2to1}')
     
     
     
-    fn1 = 'example_data/planarian.h5ad'
-    fn2 = 'example_data/schistosome.h5ad'
+    fn1 = h5ad1
+    fn2 = h5ad2
     fn3 = 'example_data/hydra.h5ad'
     
     filenames = {'pl':fn1,'sc':fn2,'hy':fn3}
@@ -106,21 +104,11 @@ def main() -> None:
     )
     
     sm.run(pairwise=True)
-    # samap = sm.samap
     
-
     
-    # print(sm.query_gene_pairs('pl_dd_Smed_v4_659_0_1'))
-    # print(sm.query_gene_pair('pl_dd_Smed_v4_659_0_1','Smp_179320'))
-    
-    keys = {'pl':'cluster','hy':'Cluster','sc':'tissue'}
-    D,MappingTable = get_mapping_scores(sm,keys,n_top = 0)
+    # keys = {'pl':'cluster','hy':'Cluster','sc':'tissue'}
+    # D,MappingTable = get_mapping_scores(sm,keys,n_top = 0)
     save_samap(sm, 'example_data/samap_output')
-    
-    # print(D.head())
-    # print(MappingTable.head())
-    
-    # run_samap(species1, h5ad1, map1to2, species2, h5ad2, map2to1)
     
     
     
