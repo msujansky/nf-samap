144991e655cf0eb0afd6b83b028416190080c22a
RyanSonder <ryansonderpersonal@gmail.com>
2025-06-10 16:44:34 -0400

    blacked

--- scripts/run_samap.py
+++ scripts/run_samap.py
@@ -14,21 +14,26 @@ from typing import NamedTuple
 
 
 class Args(NamedTuple):
-    """ Command-line arguments """
+    """Command-line arguments"""
+
     config: Path
+
 
 # --------------------------------------------------
 def get_args() -> Args:
-    """ Get command-line arguments """
+    """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Run SAMAP with command-line input',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Run SAMAP with command-line input",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('--config',
-                        type=Path,
-                        default=Path('config.json'),
-                        help='Path to JSON with species names and h5ad paths')
+    parser.add_argument(
+        "--config",
+        type=Path,
+        default=Path("config.json"),
+        help="Path to JSON with species names and h5ad paths",
+    )
 
     args = parser.parse_args()
 
@@ -37,9 +42,9 @@ def get_args() -> Args:
 
 # --------------------------------------------------
 def main() -> None:
-    """ Run SAMap with command-line input """
-    args = get_args()    
-    
+    """Run SAMap with command-line input"""
+    args = get_args()
+
     with open(args.config, "r") as f:
         config = json.load(f)
 
@@ -47,15 +52,15 @@ def main() -> None:
     data_dict = config.get("species", {})
 
     sm = SAMAP(
-        sams=data_dict, 
-        f_maps=maps_dir, 
-        save_processed=True # If False, do not save the processed results to `*_pr.h5ad`
+        sams=data_dict,
+        f_maps=maps_dir,
+        save_processed=True,  # If False, do not save the processed results to `*_pr.h5ad`
     )
 
     sm.run()
-    save_samap(sm, 'samap_obj')
+    save_samap(sm, "samap_obj")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
